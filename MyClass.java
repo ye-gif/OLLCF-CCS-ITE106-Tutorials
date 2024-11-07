e28472b1c00a64b2e3ec16cd1d07f54856bae184
ye-gif <marjoriealforte13@gmail.com>
2024-11-07 17:42:40 +0800

    Add files via upload

---
+++ MyClass.java
@@ -0,0 +1,6 @@
+public class MyClass{
+	public void getNames(String name){
+		System.out.println(name);
+	}
+	
+}
---
+++ PrintName.java
@@ -0,0 +1,9 @@
+public class PrintName{
+	
+	public static void main(String[] args){
+		String name = "GeeksforGeeks";
+		MyClass obj = new MyClass();
+		
+		obj.getNames(name);
+	}
+}
