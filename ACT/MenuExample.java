f044bbb2fe37bfedb3402c65e131bef8361b9a0f
ye-gif <marjoriealforte13@gmail.com>
2024-11-21 16:19:18 +0800

    Create MenuExample.java

---
+++ ACT/MenuExample.java
@@ -0,0 +1,33 @@
+import javax.swing.*;
+import java.awt.event.*;
+import java.util.Date;
+
+public class MenuExample {
+	public static void main(String[] args){
+		JFrame frame = new JFrame("MyFrame");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			
+		JMenu fileMenu = new JMenu("File");
+		JMenuItem item = new JMenuItem("Close");
+		item.addActionListener(new MenuActionListener());
+		fileMenu.add(item);
+		
+		fileMenu.add(new JMenuItem("New"));
+		fileMenu.add(new JMenuItem("Open"));
+		fileMenu.add(new JMenuItem("Close"));
+		
+		JMenu editMenu = new JMenu("Edit");
+		editMenu.add(new JMenuItem("Undo"));
+		editMenu.add(new JMenuItem("Redo"));
+		editMenu.add(new JMenuItem("Cut"));
+		
+		JMenuBar menubar = new JMenuBar();
+		menubar.add(fileMenu);
+		menubar.add(editMenu);
+		
+		frame.setJMenuBar(menubar);
+		
+		frame.setSize(300, 400);
+		frame.setVisible(true);
+	}
+}
